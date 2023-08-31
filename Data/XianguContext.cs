25fc756b85ad058420f5cd62bcc1b2d183bf2020
a33573 <a33573@mail.cmuh.org.tw>
2023-08-31 16:54:26 +0800

    練習ef core

---
+++ Data/XianguContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWithMud.Data
+{
+    public class XianguContext:DbContext
+    {
+        public XianguContext() { }
+        public XianguContext(DbContextOptions<XianguContext> options):base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=XianguNoOne;Trusted_connection=true;");
+        }
+
+        public DbSet<WeatherForecast> forecasts { get; set; }
+    }
+}
